8eff3866861d82728d343743ac4647d51c8e36d8
Nikita <48492775+MoriSummerz@users.noreply.github.com>
2022-03-15 10:13:23 +0300

    Update lyrics.py
    Disable web preview

--- lyrics.py
+++ lyrics.py
@@ -108,6 +108,7 @@ class LyricsMod(loader.Module):
                         + track.find("a", class_="title")["href"]
                     ),
                     parse_mode="HTML",
+                    disable_web_page_preview=True,
                 ),
                 **({"thumb_url": add_protocol(track.find("img")["srcset"].split()[-2])})
                 if "has-picture" in str(track)
