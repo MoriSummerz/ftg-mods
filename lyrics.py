14e0ab39a89070ca299383229c502b9042737e45
”nikitaa12” <timofeevnikita111@gmail.com>
2022-03-30 11:57:06 +0300

    Code reformat, add docstrings, strings

--- lyrics.py
+++ lyrics.py
@@ -1,4 +1,4 @@
-__version__ = (2, 3, 0)
+__version__ = (2, 3, 1)
 
 """"
     █▀▄▀█ █▀█ █▀█ █ █▀ █ █ █▀▄▀█ █▀▄▀█ █▀▀ █▀█
@@ -9,21 +9,20 @@ __version__ = (2, 3, 0)
 # scope: inline_content
 # requires: requests bs4 spotipy
 # meta developer: @morisummermods
+from bs4 import BeautifulSoup
+import spotipy
+import requests
 from aiogram.types import (
-    CallbackQuery,
     InlineKeyboardMarkup,
     InlineQueryResultArticle,
     InlineKeyboardButton,
     InputTextMessageContent,
 )
-import spotipy
-import logging
-from bs4 import BeautifulSoup
-from urllib.parse import quote_plus
-import requests
 from telethon.tl.types import Message  # noqa
-import re
+from urllib.parse import quote_plus
 from .. import loader  # noqa
+import logging
+import re
 
 try:
     from ..inline import GeekInlineQuery, rand  # noqa
@@ -41,13 +40,16 @@ api_headers = {
     "Host": "api.genius.com",
 }
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/99.0.4844.82 Safari/537.36"
 }
 host = "https://api.genius.com"
 n = "\n"
 
 
 def get_lyrics(song_url, remove_section_headers=False):
+    """Uses BeautifulSoup to scrape song info off of a Genius song URL"""
     page = requests.get(song_url, headers=headers)
     html = BeautifulSoup(page.text.replace("<br/>", "\n"), "html.parser")
     lyrics = "\n".join(
@@ -67,8 +69,12 @@ def get_lyrics(song_url, remove_section_headers=False):
 
 
 def search(q):
+    """Search documents hosted on Genius"""
     req = requests.get(
-        f"https://api.genius.com/search?text_format=plain&q={quote_plus(q)}&access_token=uhYUr-qrBp5V3o46lA8vcaL1DKXTWVs5SDsb_0CDCIcKxKLwtapqeqkdNu8JnA6w",
+        f"https://api.genius.com/search"
+        f"?text_format=plain"
+        f"&q={quote_plus(q)}"
+        f"&access_token=uhYUr-qrBp5V3o46lA8vcaL1DKXTWVs5SDsb_0CDCIcKxKLwtapqeqkdNu8JnA6w",
         headers=api_headers,
     ).json()
 
@@ -85,19 +91,30 @@ def search(q):
 
 
 def add_protocol(x):
+    """Add https protocol to link"""
     return x if not x.startswith("//") else f"https:{x}"
 
 
 def link(url: str) -> InlineKeyboardMarkup:
+    """InlineKeyboardButton markup generator"""
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton("🎵 Full lyrics on Genius", url=url))
     return markup
 
 
 class LyricsMod(loader.Module):
-    """Songs lyrics from Genius"""
+    """Song lyrics from Genius"""
 
-    strings = {"name": "Lyrics"}
+    strings = {
+        "name": "Lyrics",
+        "type_name": "<b>🚫 Please type name of the song</b>",
+        "genius": "🎵 Full lyrics on Genius",
+        "noSpotify": "<b>🚫 Please install SpotifyNow module and proceed auth</b>\n"
+        "🌃 Install: <code>.dlmod https://mods.hikariatama.ru/spotify.py</code>",
+        "sauth": "<b>🚫 Execute <code>.sauth</code> before using this action.</b>",
+        "SpotifyError": "<b>🚫 Spotify error</b>",
+        "noResults": "<b>🚫 No results found</b>",
+    }
 
     async def client_ready(self, client, db) -> None:
         self.db = db
@@ -115,23 +132,22 @@ class LyricsMod(loader.Module):
                         for entity in reply.entities
                         if type(entity).__name__ == "MessageEntityCode"
                     )
-                    text = reply.raw_text[e.offset - 1: e.offset + e.length]
+                    text = reply.raw_text[e.offset - 1 : e.offset + e.length]
                 except Exception:
                     text = reply.raw_text
             else:
-                await utils.answer(message, "<b>🚫 Please type name of the song</b>")
+                await utils.answer(message, self.strings["type_name"])
                 return
         tracks = search(text)
         if len(tracks) > 0:
             track = tracks[0]
         else:
-            await utils.answer(message, "<b>🚫 No results found</b>")
+            await utils.answer(message, self.strings["noResults"])
             return
-        # pic = track.find('img')['srcset'].split()[-2]
         await self.inline.form(
             f"Lyrics for <b>{track['title']}</b> by <b>{track['artists']}</b>{n}"
             f"<i>{get_lyrics(track['url'])}"[:4092] + "</i>",
-            reply_markup=[[{"text": "🎵 Full lyrics on Genius", "url": track['url']}]],
+            reply_markup=[[{"text": self.strings["genius"], "url": track["url"]}]],
             force_me=False,
             message=message,
         )
@@ -147,11 +163,11 @@ class LyricsMod(loader.Module):
                 [
                     InlineQueryResultArticle(
                         id="-1",
-                        title="No results found",
+                        title=self.strings["noResults"],
                         description="Please try again",
                         thumb_url="https://img.icons8.com/stickers/100/000000/nothing-found.png",
                         input_message_content=InputTextMessageContent(
-                            f"<b>🚫 No results found for <code>{text}</code></b>",
+                            f"{self.strings['noResults']} for <code>{text}</code>",
                             parse_mode="HTML",
                         ),
                     )
@@ -182,17 +198,18 @@ class LyricsMod(loader.Module):
         """Get lyrics from your current Spotify playback (Needs SpotifyNow module)"""
         check = self.db.get("SpotifyNow", "acs_tkn", "404")
         if check == "404":
-            await utils.answer(message, "<b>🚫 Please install SpotifyNow module and proceed auth</b>\n"
-                                        "🌃 Install: <code>.dlmod https://mods.hikariatama.ru/spotify.py</code>")
+            await utils.answer(message, self.strings["noSpotify"])
             return
         elif check is None:
-            await utils.answer(message, "<b>🚫 Execute <code>.sauth</code> before using this action.</b>")
+            await utils.answer(message, self.strings["sauth"])
             return
         try:
-            sp = spotipy.Spotify(auth=self.db.get("SpotifyNow", "acs_tkn")["access_token"])
+            sp = spotipy.Spotify(
+                auth=self.db.get("SpotifyNow", "acs_tkn")["access_token"]
+            )
             current_playback = sp.current_playback()
         except Exception:
-            await utils.answer(message, "<b>🚫 Spotify error</b>")
+            await utils.answer(message, self.strings["SpotifyError"])
             return
         try:
             track = current_playback["item"]["name"]
@@ -202,16 +219,16 @@ class LyricsMod(loader.Module):
             artists = [artist["name"] for artist in current_playback["item"]["artists"]]
         except Exception:
             artists = None
-        tracks = search(f'{artists} {track}')
+        tracks = search(f"{artists} {track}")
         if len(tracks) > 0:
             track = tracks[0]
         else:
-            await utils.answer(message, "<b>🚫 No results found</b>")
+            await utils.answer(message, self.strings["noResults"])
             return
         await self.inline.form(
             f"Lyrics for <b>{track['title']}</b> by <b>{track['artists']}</b>{n}"
             f"<i>{get_lyrics(track['url'])}"[:4092] + "</i>",
-            reply_markup=[[{"text": "🎵 Full lyrics on Genius", "url": track['url']}]],
+            reply_markup=[[{"text": self.strings["genius"], "url": track["url"]}]],
             force_me=False,
             message=message,
         )
