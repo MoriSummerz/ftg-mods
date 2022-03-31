57b197bfcf1e98336c117e450dd26140ce84ee65
”nikitaa12” <timofeevnikita111@gmail.com>
2022-03-31 10:46:20 +0300

    Add picsaver.py module

---
+++ picsaver.py
@@ -0,0 +1,63 @@
+""""
+    █▀▄▀█ █▀█ █▀█ █ █▀ █ █ █▀▄▀█ █▀▄▀█ █▀▀ █▀█
+    █ ▀ █ █▄█ █▀▄ █ ▄█ █▄█ █ ▀ █ █ ▀ █ ██▄ █▀▄
+    Copyright 2022 t.me/morisummerzxc
+    Licensed under the Apache License, Version 2.0
+"""
+# meta developer: @morisummermods
+from .. import loader, utils  # noqa
+from telethon.tl.types import Message  # noqa
+import logging
+from telethon.utils import get_display_name  # noqa
+
+logger = logging.getLogger(__name__)
+
+
+class PicsaverMod(loader.Module):
+    """ "Automatic Self-destructing media saver to Saved Messages"""
+
+    strings = {"name": "Picsaver"}
+
+    async def client_ready(self, client, db) -> None:
+        self.db = db
+        self.client = client
+        self.enable = db.get(self.strings["name"], "enable", True)
+
+    async def watcher(self, message: Message) -> None:
+        if (
+            getattr(message, "media", False)
+            and getattr(message.media, "ttl_seconds", False)
+            and self.enable
+        ):
+            media = await self.client.download_media(message.media)
+            name = get_display_name(await self.client.get_entity(message.sender_id))
+            return await self.client.send_file(
+                "me", media, caption=f"Self-destructing media from {name}"
+            )
+
+    async def spcmd(self, message: Message):
+        """Reply to self-destructing media to save"""
+        reply = await message.get_reply_message()
+        if (
+            not reply
+            or not getattr(reply, "media", False)
+            or not getattr(reply.media, "ttl_seconds", False)
+        ):
+            return await message.edit("Reply for self-destructing media !")
+        await message.delete()
+        media = await self.client.download_media(reply.media)
+        name = get_display_name(await self.client.get_entity(reply.sender_id))
+        return await self.client.send_file(
+            "me", media, caption=f"Self-destructing media from {name}"
+        )
+
+    async def pscmd(self, message: Message):
+        """Enable/disable automatic self-destructing media save"""
+        if self.enable:
+            self.enable = False
+            self.db.set(self.strings["name"], "enable", False)
+            return await utils.answer(message, "<b>Autosave is disabled</b>")
+        else:
+            self.enable = True
+            self.db.set(self.strings["name"], "enable", True)
+            return await utils.answer(message, "<b>Autosave is enabled</b>")
